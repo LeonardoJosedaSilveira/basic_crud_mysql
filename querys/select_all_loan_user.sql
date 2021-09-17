c4e9aa11268fa6bc04b82990a30931f04322c084
Leonardo Jose da Silveira <rpgleo.js@gmail.com>
2021-09-17 12:49:38 -0300

    Update select_all_loan_user.sql

--- querys/select_all_loan_user.sql
+++ querys/select_all_loan_user.sql
@@ -1,21 +1,21 @@
 USE `library`;
 
 SELECT 
-	`user_name`,
+    `user_name`,
     `book_name`,
     `date_loan`,
-	`date_back`
+    `date_back`
     
 FROM
-	`users`
+    `users`
 
 INNER JOIN 
-	`loan` USING(`user_id`)
+    `loan` USING(`user_id`)
 INNER JOIN
-	`books` USING(`book_id`)
+    `books` USING(`book_id`)
     
 WHERE
-	`user_id` = '1'
+    `user_id` = '1'
     
 ORDER BY
-	`date_loan`;
+    `date_loan`;
